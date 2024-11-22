0d2ce0297b79820f405e36591391e0fbadc37008
giovanimf <giovani.mf@hotmail.com>
2024-11-22 03:37:47 -0300

    version 3.0

--- src/app/api/jogador/route.ts
+++ src/app/api/jogador/route.ts
@@ -4,11 +4,26 @@ import { NextRequest, NextResponse } from "next/server";
 
 // Rota para listar todos os jogadores
 export async function GET(request: NextRequest) {
-
   try {
     const jogadores = await prisma.jogador.findMany();
-    return NextResponse.json(jogadores);
+
+    return new NextResponse(JSON.stringify(jogadores), {
+      status: 200,
+      headers: {
+        "Cache-Control": "no-store, max-age=0", // Garante que os dados não sejam armazenados em cache
+        "Content-Type": "application/json",
+      },
+    });
   } catch (error) {
-    return NextResponse.json({ error: "Erro: Erro ao buscar jogadores." }, { status: 500 });
+    return new NextResponse(
+      JSON.stringify({ error: "Erro: Erro ao buscar jogadores." }),
+      {
+        status: 500,
+        headers: {
+          "Cache-Control": "no-store, max-age=0", // Mesmo em erros, não cacheia
+          "Content-Type": "application/json",
+        },
+      }
+    );
   }
 }
